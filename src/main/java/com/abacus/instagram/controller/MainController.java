4b793d4829e23630573a7b33ea08d9375504683c
r1605866 <r1605866@naver.com>
2023-07-23 18:15:12 +0900

    project Upload

---
+++ src/main/java/com/abacus/instagram/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.abacus.instagram.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/api/main")
+    public List<String> MainList(){
+        return Arrays.asList("Main", "Main");
+    }
+}
