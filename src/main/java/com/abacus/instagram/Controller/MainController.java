716a48b497a2d2ad5dc4feb094ffe5273798e8e4
r1605866 <r1605866@naver.com>
2023-07-23 20:14:56 +0900

    Jpa implementation add

--- src/main/java/com/abacus/instagram/Controller/MainController.java
+++ src/main/java/com/abacus/instagram/Controller/MainController.java
@@ -1,4 +1,4 @@
-package com.abacus.instagram.controller;
+package com.abacus.instagram.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
